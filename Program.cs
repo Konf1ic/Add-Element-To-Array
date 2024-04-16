6cf8b96124fa45dd8b8e63e1b423cef6e9d33f2c
Konf1ic <kenbui2809@gmail.com>
2024-04-16 13:45:10 +0700

    AddEToArray

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[10] { 10, 4, 6, 7, 8, 0, 0, 0, 0, 0 };
+
+        Console.Write("X that you want to add: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Index position want to add: ");
+        int index = int.Parse(Console.ReadLine());
+
+        // Check index
+        if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index not avalible!");
+            return;
+        }
+
+        // Add X
+        for (int i = array.Length - 1; i > index; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[index] = x;
+
+        Console.WriteLine("New array:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
